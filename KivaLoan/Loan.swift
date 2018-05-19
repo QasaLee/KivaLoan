c4d1a9d5d8972c223f3495dfef595957ad2f801a
Nate <intoqasa@gmail.com>
2018-05-19 23:41:26 +0800

    Initial Commit

---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,17 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by M78的星际人士 on 5/19/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+    
+}
